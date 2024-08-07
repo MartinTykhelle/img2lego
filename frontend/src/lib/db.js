0f9b560f3b37c8a8a0b1e1611da98b2fea1b9bd7
Martin Tykhelle <martintykhelle@gmail.com>
2024-08-07 12:27:57 +0200

    Hue

--- frontend/src/lib/db.js
+++ frontend/src/lib/db.js
@@ -2,6 +2,6 @@ import Dexie from 'dexie';
 
 export const db = new Dexie('legoDatabase');
 db.version(1).stores({
-    legoColors: '++colorCode, r, g, b, name, enabled',
+    legoColors: '++colorCode, r, g, b, name, enabled, hue',
     legoImage: '++id, filename, width, height, *buffer, *colorCounts',
 });
